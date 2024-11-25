6e9f09ed6caca1f358136764e4d294efa4008f7b
Adrián <97130439+Galveill@users.noreply.github.com>
2024-11-25 13:45:07 +0100

    Estelas de Nabu y Enki.

--- register.php
+++ register.php
@@ -171,7 +171,8 @@
 		["code" => "SUM_EST_003", "name" => "Código de Hammurabi", "description" => "Enorme estela con las leyes de la antigua Mesopotamia basadas en la ley del Talión.", "rarity" => "Común", "parts" => 48, "points" => 1],
 		["code" => "SUM_EST_004", "name" => "Estela de Ishtar", "description" => "Diosa alada del amor, la guerra, la vida y la fertilidad.", "rarity" => "Común", "parts" => 96, "points" => 1],
 		["code" => "SUM_EST_005", "name" => "Estela de Marduk", "description" => "Dios soberano de la humanidad representado como un dragón.", "rarity" => "Común", "parts" => 96, "points" => 1],
-		["code" => "SUM_EST_006", "name" => "Estela de Nabu", "description" => "Dios alado de la escritura.", "rarity" => "Común", "parts" => 96, "points" => 1]
+		["code" => "SUM_EST_006", "name" => "Estela de Nabu", "description" => "Dios alado de la escritura.", "rarity" => "Común", "parts" => 96, "points" => 1],
+		["code" => "SUM_EST_008", "name" => "Estela de Enki", "description" => "El señor de la tierra, dios que creó a los hombres y les enseñó los oficios y artes.", "rarity" => "Común", "parts" => 96, "points" => 1]
 	];
 	excelToInfo($sumeria);
 
