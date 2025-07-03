1c2f2e02dddd876c5131f4ce37ce846dc00f5985
Adrián <97130439+Galveill@users.noreply.github.com>
2025-07-03 20:32:53 +0200

    Corregida la descripción.

--- views/base.php
+++ views/base.php
@@ -4,7 +4,7 @@
 	<div>
 		<h5>Museo del sistema Archaios</h5>
 		<p>Esta es la principal fuente de información de tesoros y estado del juego.</p>
-		<p>Los tesoros encontrados en el sistema Archaios son entregados al Museo mediante Hermes. Todos los grupos podrán colaborar para intentar encontrarlos, viendo aquí el progreso de todos y cada uno.</p>
+		<p>Los tesoros encontrados en el sistema Archaios son entregados al Museo mediante Terra. Todos los grupos podrán colaborar para intentar encontrarlos, viendo aquí el progreso de todos y cada uno.</p>
 		<hr>
 		<?php
 			echo RegionsHelper::getBaseRegionsDesc();
